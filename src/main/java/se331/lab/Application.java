342f0df95e16fefe9d4a647e7b4a996628fad58e
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-09-20 02:07:25 +0700

    6.2

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -7,22 +7,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173")
+				registry.addMapping("/**")  // Apply CORS to all endpoints
+						.allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173")
 						.exposedHeaders("x-total-count");
+
+//
 			}
 		};
 	}
+
 }
